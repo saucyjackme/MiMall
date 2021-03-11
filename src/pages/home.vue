2fc478d1aeadc252a0b9624bd413792a8d321d87
saucyjackme <saucyjackyme@163.com>
2021-03-11 11:46:41 +0800

    main

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
+    home
     <nav-header></nav-header>
     <router-view></router-view>
     <nav-footer></nav-footer>
   </div>
 </template>
 <script>
+//导入组件并加载
+  import NavHeader from '../components/NavHeader'
+  import NavFooter from '../components/NavFooter'
+
   export default {
-    name:'home'
+    name:'nav-home',
+    components:{
+      NavHeader,
+      NavFooter
+    }
   }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,1 +1,11 @@
-// index.js会嵌套在home.vue的router-view中
+<template>
+  <div>
+    index
+  </div>
+</template>
+<script>
+//index.js会嵌套在home.vue的router-view中
+  export default {
+    name:'index'
+  }
+</script>

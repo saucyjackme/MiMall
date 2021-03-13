7a5461d044026b1e5b867c0728ac721062bbacec
saucyjackme <saucyjackyme@163.com>
2021-03-13 22:51:25 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,20 @@
 </template>
 
 <script>
+import storage from './storage/index'
 
 export default {
   name: 'App',
   components: {
-    
+  },
+  mounted(){
+    // storage.setItem('a',1);
+    // storage.setItem('user',{a:1});
+    storage.setItem('abc',{a:1},'user')
+    storage.clear('a');
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/storage/index.js
@@ -0,0 +1,36 @@
+// Storage封装
+const STORAGE_KEY = 'mall';
+export default {
+  //存储值
+  setItem(key, value, module_name) {
+    if (module_name) {
+      let val = this.getItem(module_name);
+      val[key] = value;
+      this.setItem(module_name, val);
+    } else {
+      let val = this.getStorage();
+      val[key] = value;
+      window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+    }
+  },
+  //获取某一模块下属性，user 下面的userName
+  getItem(key, module_name) {
+    if (module_name) {
+      let val = this.getItem(module_name);
+      if (val) return val[key];
+    }
+    return this.getStorage()[key];
+  },
+  getStorage() {
+    return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
+  },
+  clear(key,module_name) {
+    let val = this.getStorage();
+    if(module_name){
+      delete val[module_name][key];
+    }else{ 
+      delete val[key];
+    }
+    window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+  }
+}

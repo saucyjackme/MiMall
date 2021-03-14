32130ee954ea0b3f5b3dd93f62f1b2aeafd9555f
saucyjackme <saucyjackyme@163.com>
2021-03-14 19:32:56 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import routers from './router'
+import axios from 'axios'
+//VueAxios将axios挂载至vue上
+import VueAxios from 'vue-axios'
+//拦截器
+// 设置默认值，根据前端的跨域方式作调整
+axios.defaults.baseURL = '/api';
+//超时处理
+axios.defaults.timeout = 8000;
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0) {
+    return res.data;
+  }else if(res.status == 10) {
+    window.location.href = '/#/login'
+  }else {
+    alert(res.msg);
+  }
+});
 
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({

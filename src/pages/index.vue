7ed26d0d9ea195f1a1771ae7d7317c9be2719abe
saucyjackme <saucyjackyme@163.com>
2021-03-30 10:45:09 +0800

    首页手机商品实现2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -82,17 +82,17 @@
           <div class="list-box">
             <div class="list" v-for="(arr, i) in phoneList" v-bind:key="i">
               <div class="item" v-for="(item, j) in arr" v-bind:key="j">
-                <span>新品</span>
+                <span v-bind:class="{'new-pro':j%2==0}">新品</span>
                 <div class="item-img">
                   <img
-                    src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6f2493e6c6fe8e2485c407e5d75e3651.jpg"
+                    v-bind:src="item.mainImage"
                     alt=""
                   />
                 </div>
                 <div class="item-info">
-                  <h3>小米9</h3>
-                  <p>骁龙855</p>
-                  <p class="price">2999元</p>
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price">{{item.price}}元</p>
                 </div>
               </div>
             </div>
@@ -213,6 +213,23 @@ export default {
       ],
     };
   },
+  mounted(){
+    this.init();
+  },
+  methods:{
+    init() {
+      this.axios.get('/products',{
+        params:{
+          categoryId:100012,
+          pageSize:8
+        }
+      }).then((res)=>{
+        //将res结果分割成二维数组并赋值phoneList
+        // console.log(res.list);
+        this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
+      })
+    }
+  }
 };
 </script>
 <style lang="scss">
@@ -343,9 +360,22 @@ export default {
             background-color: $colorG;
             text-align: center;
             span {
+              display: inline-block;
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+              &.new-pro {
+                background-color: #7ecf86;
+              }
+              &.kill-pro {
+                background-color: #e82626;
+              }
             }
             .item-img {
               img {
+                width: 100%;
                 height: 195px;
               }
             }

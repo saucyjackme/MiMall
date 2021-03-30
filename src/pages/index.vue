14d63c08a0fc17dc7a435070e1e40663541c875b
saucyjackme <saucyjackyme@163.com>
2021-03-30 09:47:29 +0800

    首页手机商品实现1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,10 @@
                 <ul v-for="(item, i) in menuList" v-bind:key="i">
                   <li v-for="(sub, j) in item" v-bind:key="j">
                     <a v-bind:href="sub ? '/#/product/' + sub.id : ''">
-                      <img v-bind:src="sub ? sub.img : '/imgs/item-box-1.png'" alt=""/>
+                      <img
+                        v-bind:src="sub ? sub.img : '/imgs/item-box-1.png'"
+                        alt=""
+                      />
                       {{ sub ? sub.name : "小米9" }}
                     </a>
                   </li>
@@ -53,16 +56,49 @@
         </swiper>
       </div>
       <div class="ads-box">
-       <a v-bind:href="'/#/product/' + item.id" v-for="(item,index) in adsList" v-bind:key="index">
-         <img v-bind:src="item.img" alt="">
-      </a> 
+        <a
+          v-bind:href="'/#/product/' + item.id"
+          v-for="(item, index) in adsList"
+          v-bind:key="index"
+        >
+          <img v-bind:src="item.img" alt="" />
+        </a>
       </div>
       <div class="banner">
         <a href="/#/product/30">
-         <img src="/imgs/banner-1.png" alt="">
-        </a> 
+          <img src="/imgs/banner-1.png" alt="" />
+        </a>
       </div>
-      <div class="product-box"></div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/30">
+              <img src="/imgs/mix-alpha.jpg" alt="" />
+            </a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr, i) in phoneList" v-bind:key="i">
+              <div class="item" v-for="(item, j) in arr" v-bind:key="j">
+                <span>新品</span>
+                <div class="item-img">
+                  <img
+                    src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6f2493e6c6fe8e2485c407e5d75e3651.jpg"
+                    alt=""
+                  />
+                </div>
+                <div class="item-info">
+                  <h3>小米9</h3>
+                  <p>骁龙855</p>
+                  <p class="price">2999元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -155,22 +191,26 @@ export default {
       ],
       adsList: [
         {
-          id:33,
-          img: '/imgs/ads/ads-1.png'
+          id: 33,
+          img: "/imgs/ads/ads-1.png",
         },
         {
-          id:48,
-          img: '/imgs/ads/ads-2.jpg'
+          id: 48,
+          img: "/imgs/ads/ads-2.jpg",
         },
         {
-          id:45,
-          img: '/imgs/ads/ads-3.png'
+          id: 45,
+          img: "/imgs/ads/ads-3.png",
         },
         {
-          id:47,
-          img: '/imgs/ads/ads-4.jpg'
+          id: 47,
+          img: "/imgs/ads/ads-4.jpg",
         },
-      ]
+      ],
+      phoneList: [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ],
     };
   },
 };
@@ -233,7 +273,7 @@ export default {
                 padding-left: 23px;
               }
               a {
-                color:$colorB;
+                color: $colorB;
                 font-size: 14px;
               }
               img {
@@ -269,6 +309,75 @@ export default {
   }
   .banner {
     margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding-top: 30px 0 50px;
+    h2 {
+      font-size: $fontF;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img {
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+            }
+            .item-img {
+              img {
+                height: 195px;
+              }
+            }
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: $fontJ;
+                font-weight: bold;
+              }
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+              .price {
+                color: #f20a0a;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer;
+                &::after {
+                  @include bgImg(22px, 22px, "/imgs/icon-cart-hover.png");
+                  content: "";
+                  margin-left: 5px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>

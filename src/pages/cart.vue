8c55a67378f4552cbd92b1325072dc37c04971c1
saucyjackme <saucyjackyme@163.com>
2021-04-19 15:29:35 +0800

    OrderHeader头部组件插槽slot及复用

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <order-header></order-header>
-    cart
+    <order-header title="我的购物车">
+      <template v-slot:tip>
+      <span>温馨提示：产品是否购买成功，以最终下单为准，请尽快结算</span>
+      </template>
+    </order-header>
     <nav-footer></nav-footer>
   </div>
 </template>
 <script>
-import OrderHeader from '.././components/OrderHeader'
-import NavFooter from '.././components/NavFooter'
-  export default {
-    name:'cart',
-    components:{
-      OrderHeader,
-     NavFooter
-    }
-  }
+import OrderHeader from ".././components/OrderHeader";
+import NavFooter from ".././components/NavFooter";
+export default {
+  name: "cart",
+  components: {
+    OrderHeader,
+    NavFooter,
+  },
+};
 </script>

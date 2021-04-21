bf61c4e4875c3286cb10d306d59be6a4054e7bd9
saucyjackme <saucyjackyme@163.com>
2021-04-21 09:40:53 +0800

    引入babelrc以及element-ui

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -31,6 +31,7 @@ export default {
 <style lang='scss'>
 .order-header {
   padding: 30px 0;
+  border-bottom: 2px solid #ff6600;
   .header-logo {
     float: left;
   }
@@ -45,11 +46,15 @@ export default {
     font-size: 28px;
     float: left;
     color: #333333;
+    h2 {
+      font-size: 28px;
+      color:#333333;
+    }
     span {
       font-size: 14px;
       margin-left: 17px;
       color: #999999;
-      font-weight: bold;
+      font-weight: 200;
     }
   }
   .username {
